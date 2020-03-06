ddfa9be9028993a1e2d433b398e3e6c9734a1ab1
blumaa <blumaa@gmail.com>
2020-03-06 17:02:58 +0100

    fixed key prob

--- src/App.js
+++ src/App.js
@@ -1,20 +1,96 @@
 import React, { useEffect, useState } from "react";
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink
+} from "apollo-boost";
 import { gql } from "apollo-boost";
 
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+console.log(API_KEY)
 function App() {
-  const [repo, setRepo] = useState({})
+  const [repo, setRepo] = useState({});
 
   console.log("app loaded");
 
-  useEffect(() =>{
- 
+  useEffect(() => {
+    const httpLink = new HttpLink({ uri: "https://api.github.com/graphql" });
+
+    const authLink = new ApolloLink((operation, forward) => {
+      // Retrieve the authorization token from local storage.
+      const token = API_KEY;
+
+      // Use the setContext method to set the HTTP headers.
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}` : ""
+        }
+      });
+
+      // Call the next link in the middleware chain.
+      return forward(operation);
+    });
+
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink), // Chain it with the HttpLink
+      cache: new InMemoryCache()
+    });
+
+    try {
+      client
+      .query({
+        query: gql`
+          {
+            repository(owner: "nuwave", name: "lighthouse") {
+              description
+              url
+              pullRequests(states: [OPEN, CLOSED], first: 20) {
+                totalCount
+                nodes {
+                  title
+                  state
+                  body
+                  createdAt
+                }
+              }
+              issues(states: [OPEN, CLOSED], first: 20) {
+                totalCount
+                nodes {
+                  title
+                  state
+                  url
+                  comments(first: 20) {
+                    nodes {
+                      url
+                      body
+                      createdAt
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `
+      })
+      .then(result => {
+        console.log(result)
+        setRepo(result)
+      });
+    } catch (error) {
+      console.log(error)
+    }
+    
+
     // fetch("BookApiUrl")
     //   .then(res => res.json())
     //   .then(setBook)
- 
-  }, [])
+  }, []);
 
+
+  console.log(repo)
   return (
     <div className="main">
       <h1 className="title">Search for a repo on Github</h1>
@@ -28,4 +104,3 @@ function App() {
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,74 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
-import { gql } from "apollo-boost";
-// import gql from 'graphql-tag'
-
-// const client = new ApolloClient({
-//   uri: "https://api.github.com/graphql"
-// });
-
-
-const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' });
-
-const authLink = new ApolloLink((operation, forward) => {
-  // Retrieve the authorization token from local storage.
-  const token = '0012d7abd79599e50f846f422b9df73ff5b86522'
-
-  // Use the setContext method to set the HTTP headers.
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  });
-
-  // Call the next link in the middleware chain.
-  return forward(operation);
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink), // Chain it with the HttpLink
-  cache: new InMemoryCache()
-});
-
-
-client
-  .query({
-    query: gql`
-      {
-        repository(owner: "nuwave", name: "lighthouse") {
-          description
-          url
-          pullRequests(states: [OPEN, CLOSED], first: 20) {
-            totalCount
-            nodes {
-              title
-              state
-              body
-              createdAt
-            }
-          }
-          issues(states: [OPEN, CLOSED], first: 20) {
-            totalCount
-            nodes {
-              title
-              state
-              url
-              comments(first: 20) {
-                nodes {
-                  url
-                  body
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -78,6 +10,5 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
 
 // 0012d7abd79599e50f846f422b9df73ff5b86522
