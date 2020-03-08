75ceee829fcc015b96352bda8918b1aa0a97e1a2
blumaa <blumaa@gmail.com>
2020-03-08 15:08:58 +0100

    comment filter implemented

--- src/components/ClosedIssuesDisplay.js
+++ src/components/ClosedIssuesDisplay.js
@@ -1,26 +1,44 @@
-import React from "react"
-import { Header, Card } from 'semantic-ui-react'
+import React from "react";
+import { Header, Card, Modal, Button, Icon } from "semantic-ui-react";
+import IssueDetails from "./IssueDetails";
 
-const OpenIssuesDisplay = ({repository}) => {
-    // console.log('pull req repo', repository)
-    const filteredRepos = repository.issues.nodes.filter(node => node.state === "CLOSED")
-    // console.log(filteredRepos)
-    const issues = filteredRepos.map(iss => {
-        // console.log(iss)
-        return   <Card
-        style={{width: "100%"}}
+const OpenIssuesDisplay = ({ repository }) => {
+  const issDetails = iss => {
+    console.log("issue details", iss);
+  };
+
+  // console.log('pull req repo', repository)
+  const filteredRepos = repository.issues.nodes.filter(
+    node => node.state === "CLOSED"
+  );
+  // console.log(filteredRepos)
+  const issues = filteredRepos.map(iss => {
+    console.log(iss);
+    return (
+      <Modal
         key={iss.id}
-        header={iss.title}
-        meta={iss.createdAt}
-        description={iss.state}
-        description={iss.body}
-      />
-    })
-    return(
-        <div>
-            <div>{issues}</div>
-        </div>
-    )
-}
+        trigger={
+          <Card
+            style={{ width: "100%" }}
+            key={iss.id}
+            header={iss.title}
+            meta={iss.createdAt}
+            description={iss.state}
+            description={iss.body}
+          />
+        }
+      >
+        <Modal.Content>
+          <IssueDetails iss={iss} />
+        </Modal.Content>
+      </Modal>
+    );
+  });
+  return (
+    <div>
+      <div>{issues}</div>
+    </div>
+  );
+};
 
-export default OpenIssuesDisplay
+export default OpenIssuesDisplay;
--- src/components/IssueDetails.js
+++ src/components/IssueDetails.js
@@ -3,9 +3,13 @@ import { Card, List } from "semantic-ui-react";
 
 const IssueDetails = ({ iss }) => {
   const [issue, setIssue] = useState(iss);
-  console.log(issue);
+  const [comments, setComments ] = useState(issue.comments.nodes)
+  const [ filter, setFilter ] = useState()
+//   console.log(issue);
 
-  let filteredComments = issue.comments.nodes.map(comment => {
+  let sortedComments = filter ? filter.sort((a, b) => b.createdAt - a.createdAt) : comments.sort((a, b) => b.createdAt - a.createdAt)
+
+  let renderComments = sortedComments.map(comment => {
     return (<List.Item key={comment.id}>
         <List.Content>
             <List.Description>{comment.body}</List.Description>
@@ -15,12 +19,22 @@ const IssueDetails = ({ iss }) => {
   });
 
   const filterComments = e => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    const filteredComments = comments.filter(comment => {
+        return comment.body.includes(e.target.value)
+    })
+    filteredComments.sort((a,b)=> b.createdAt - a.createdAt)
+    if (!e.target.value) {
+        setComments(iss.comments.nodes)
+    } else {
+    setFilter(filteredComments)
+    }
+    // console.log(comments)
   };
 
   return (
     <>
-      <Card>
+      <Card style={{width: "100%"}}>
         <Card.Content>
           <Card.Header>{iss.title}</Card.Header>
           <Card.Meta>{iss.createdAt}</Card.Meta>
@@ -34,7 +48,7 @@ const IssueDetails = ({ iss }) => {
             onChange={filterComments}
             placeholder="filter search"
           />
-          <List>{filteredComments}</List>
+          <List>{renderComments}</List>
         </Card.Content>
       </Card>
     </>
